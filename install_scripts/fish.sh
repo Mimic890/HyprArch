be93aeb26e22f671849d69569a4c550e2a287a75
Mimic890 <144267059+Mimic890@users.noreply.github.com>
2025-07-04 07:40:53 +0000

    fix wallpapers and installation output

--- install_scripts/fish.sh
+++ install_scripts/fish.sh
@@ -8,9 +8,9 @@ sudo pacman -S --noconfirm fish >>"$HOME/HyprArch/log.txt" 2>&1 || {
 
 # Set fish as the default shell for the current user
 if ! grep -q "/usr/bin/fish" /etc/shells; then
-    echo "/usr/bin/fish" | sudo tee -a /etc/shells >>"$HOME/HyprArch/log.txt" 2>&1
+    echo "/usr/bin/fish" | sudo tee -a /etc/shells >>"$HOME/HyprArch/log.txt"
 fi
-chsh -s /usr/bin/fish >>"$HOME/HyprArch/log.txt" 2>&1 || {
+chsh -s /usr/bin/fish >>"$HOME/HyprArch/log.txt" || {
     echo -e "\e[31m❌ Failed to set fish as the default shell.\e[0m"
     exit 1
 }
@@ -19,7 +19,7 @@ chsh -s /usr/bin/fish >>"$HOME/HyprArch/log.txt" 2>&1 || {
 echo -e "\e[34mCopying fish configuration...\e[0m"
 mkdir -p "$HOME/.config"
 if [ -d "$HOME/HyprArch/customs/fish" ]; then
-    cp -r "$HOME/HyprArch/customs/fish" "$HOME/.config/" >>"$HOME/HyprArch/log.txt" 2>&1
+    cp -r "$HOME/HyprArch/customs/fish" "$HOME/.config/" >>"$HOME/HyprArch/log.txt"
 else
     echo -e "\e[31m❌ Fish config directory not found.\e[0m"
     exit 1
