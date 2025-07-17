8ccc7e93411c46ce5a7bc94d0639d85e285ef4b4
Mimic890 <144267059+Mimic890@users.noreply.github.com>
2025-07-17 14:10:36 +0300

    nwg-dock-hyprland update styles

--- configs/nwg-dock-hyprland/launch.sh
+++ configs/nwg-dock-hyprland/launch.sh
@@ -3,17 +3,9 @@
 #  / // / _ \/ __/  '_/
 # /____/\___/\__/_/\_\
 #
-config="$HOME/.config/gtk-3.0/settings.ini"
-if [ ! -f $HOME/.config/ml4w/settings/dock-disabled ]; then
-    killall nwg-dock-hyprland
-    sleep 0.5
-    prefer_dark_theme="$(grep 'gtk-application-prefer-dark-theme' "$config" | sed 's/.*\s*=\s*//')"
-    if [ $prefer_dark_theme == 0 ]; then
-        style="style-light.css"
-    else
-        style="style-dark.css"
-    fi
-    nwg-dock-hyprland -i 32 -w 5 -mb 10 -ml 10 -mr 10 -x -s $style -c  "wofi -show drun"
-else
-    echo ":: Dock disabled"
-fi
+config="$HOME/.config/gtk-4.0/settings.ini"
+killall nwg-dock-hyprland
+sleep 0.5
+prefer_dark_theme="$(grep 'gtk-application-prefer-dark-theme' "$config" | sed 's/.*\s*=\s*//')"
+style="style.css"
+nwg-dock-hyprland -i 40 -w 10 -mb 3 -ml 10 -mr 10 -x -s $style -c  "wofi --show drun"
